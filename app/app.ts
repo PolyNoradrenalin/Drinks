3fee4b47893818001f3555970f918f092e1043e7
NarvinChana <narvin.chana@etu.univ-tours.fr>
2022-01-03 16:18:57 +0100

    feat: added initial connection to db

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,15 @@
 const { Sequelize } = require('sequelize');
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection( {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "drinks"
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+})
