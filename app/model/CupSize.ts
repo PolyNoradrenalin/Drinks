c2a316a7f179b2ccd66d1c4ee6017ce1c3bd4b14
NarvinChana <narvin.chana@etu.univ-tours.fr>
2022-01-03 17:10:37 +0100

    fix: simplified CupSize

--- app/model/CupSize.ts
+++ app/model/CupSize.ts
@@ -2,12 +2,6 @@ import { Table, Column, Model } from 'sequelize-typescript';
 
 @Table
 class CupSize extends Model<CupSize> {
-
     @Column
     private size!: number;
-
-    constructor(size: number) {
-        super();
-        this.size = size;
-    }
 }
