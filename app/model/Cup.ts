cdfe89d3014e0e582a37aa41cfc81011285d618e
NarvinChana <narvin.chana@etu.univ-tours.fr>
2022-01-20 00:43:26 +0100

    feat: code in app.ts now works, the model is probably correct now

--- app/model/Cup.ts
+++ app/model/Cup.ts
@@ -6,48 +6,18 @@ import { DrinkOrder } from './DrinkOrder';
  */
 @Table
 export class Cup extends Model<Cup> {
-    // Attributes
     @Column({
         type: DataType.DECIMAL,
         comment: "Currency in Euros"
     })
-    private _price!: number;
+    price: number;
 
     @Column(DataType.INTEGER)
-    private _stock!: number;
+    stock: number;
 
     @Column(DataType.INTEGER)
-    private _size!: number;
+    size: number;
 
     @HasMany(() => DrinkOrder)
-    _drinkOrder!: DrinkOrder[];
-
-    // Accessors
-    public get price(): number {
-        return this._price;
-    }
-    public set price(value: number) {
-        this._price = value;
-    }
-
-    public get stock(): number {
-        return this._stock;
-    }
-    public set stock(value: number) {
-        this._stock = value;
-    }
-
-    public get size(): number {
-        return this._size;
-    }
-    public set size(value: number) {
-        this._size = value;
-    }
-
-    public get drinkOrder(): DrinkOrder[] {
-        return this._drinkOrder;
-    }
-    public set drinkOrder(value: DrinkOrder[]) {
-        this._drinkOrder = value;
-    }
+    drinkOrder: DrinkOrder[] = [];
 }
--- app/model/DrinkOrder.ts
+++ app/model/DrinkOrder.ts
@@ -2,7 +2,6 @@ import {Table, Column, Model, PrimaryKey, HasOne, DataType, BelongsTo, ForeignKe
 import { Cup } from './Cup';
 import { Drink } from './Drink';
 import { Resource } from './Resource';
-import { UsesResource } from './UsesResource';
 
 /**
  * Represents a drink order.
@@ -11,37 +10,26 @@ import { UsesResource } from './UsesResource';
 @Table
 export class DrinkOrder extends Model<DrinkOrder> {
     @Column(DataType.TINYINT)
-    private _canceled!: boolean;
+    canceled: boolean;
 
     @Column(DataType.TINYINT)
-    private _bought_cup!: boolean;
+    bought_cup: boolean;
+
+    @Column({
+        type: DataType.DECIMAL,
+        comment: "Currency in Euros"
+    })
+    price: number;
 
     @BelongsTo(() => Cup)
-    size!: Cup;
+    cup: Cup;
 
     @ForeignKey(() => Cup)
-    cupId!: number;
+    cupId: number;
 
     @BelongsTo(() => Drink)
-    drink!: Drink;
+    drink: Drink;
 
     @ForeignKey(() => Drink)
-    drinkId!: number;
-
-    @BelongsToMany(() => Resource, () => UsesResource)
-    resources!: Resource[];
-
-    public get canceled(): boolean {
-        return this._canceled;
-    }
-    public set canceled(value: boolean) {
-        this._canceled = value;
-    }
-
-    public get bought_cup(): boolean {
-        return this._bought_cup;
-    }
-    public set bought_cup(value: boolean) {
-        this._bought_cup = value;
-    }
+    drinkId: number;
 }
