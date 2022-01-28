d3f9f88ddff2aae0b4a6bf39246dda23a9e91a20
Banalian <64379456+Banalian@users.noreply.github.com>
2022-01-28 12:27:11 +0100

    chore: refactored tests to be inside a general file scope
    test: added displayMessage tests

--- tests/OrderBuilderTests.ts
+++ tests/OrderBuilderTests.ts
@@ -10,168 +10,159 @@ const assert = require("assert");
 import { OrderBuilder } from "../src/controller/OrderBuilder";
 import {Cup} from "../src/entity/Cup";
 
-let consoleStub;
 
-describe("setDrink", function () {
+
+
+describe("OrderBuilder", function (){
+    let consoleStub;
     let orderBuilder: OrderBuilder;
-    beforeEach(function () {
+    beforeEach(function (){
+        consoleStub = sinon.stub(console, "log");
         orderBuilder = new OrderBuilder();
     });
 
-    it("should set the drink", function () {
-        let drink = new Drink();
-        drink.name = "Test";
-        orderBuilder.setDrink(drink);
-        assert.equal(orderBuilder.getOrder().drink, drink);
+    afterEach(function (){
+        consoleStub.restore();
     });
 
-    it("should throw an error if the drink is null", function () {
-        let drink = null;
-        assert.throws(() => {
+    describe("setDrink", function () {
+
+        it("should set the drink", function () {
+            let drink = new Drink();
+            drink.name = "Test";
             orderBuilder.setDrink(drink);
+            assert.equal(orderBuilder.getOrder().drink, drink);
         });
+
+        it("should throw an error if the drink is null", function () {
+            let drink = null;
+            assert.throws(() => {
+                orderBuilder.setDrink(drink);
+            });
+        });
+
+        it("should throw an error if the drink is undefined", function () {
+            let drink;
+            assert.throws(() => {
+                orderBuilder.setDrink(drink);
+            });
+        });
+
     });
 
-    it("should throw an error if the drink is undefined", function () {
-        let drink;
-        assert.throws(() => {
+
+    describe("setCup", function () {
+
+        it("should set the cup", function () {
+            let cup = new Cup();
+            orderBuilder.setCup(cup);
+            assert.equal(orderBuilder.getOrder().cup, cup);
+        });
+
+        it("should throw an error if the cup is null", function () {
+            let cup = null;
+            assert.throws(() => {
+                orderBuilder.setCup(cup);
+            });
+        });
+
+        it("should throw an error if the cup is undefined", function () {
+            let cup;
+            assert.throws(() => {
+                orderBuilder.setCup(cup);
+            });
+        });
+
+    });
+
+
+    describe("setCupChoice", function () {
+
+        it("should set the cup choice boolean", function () {
+            let cupChoice = true;
+            orderBuilder.setCupChoice(cupChoice);
+            assert.equal(orderBuilder.getOrder().bought_cup, cupChoice);
+
+            cupChoice = false;
+            orderBuilder.setCupChoice(cupChoice);
+            assert.equal(orderBuilder.getOrder().bought_cup, cupChoice);
+        });
+
+        it("should throw an error if the choice is null", function () {
+            let cupChoice = null;
+            assert.throws(() => {
+                orderBuilder.setCupChoice(cupChoice);
+            });
+        });
+
+        it("should throw an error if the choice is undefined", function () {
+            let cupChoice;
+            assert.throws(() => {
+                orderBuilder.setCupChoice(cupChoice);
+            });
+        });
+
+    });
+
+
+    describe("setSugarChoice", function () {
+
+        it("should set the sugar amount", function () {
+            let sugar = 2;
+            orderBuilder.setSugarChoice(sugar);
+            assert.equal(orderBuilder.getOrder().sugarAmount, sugar);
+        });
+
+        it("should throw an error if the amount is above 5", function () {
+            let sugar = 6;
+            assert.throws(() => {
+                orderBuilder.setSugarChoice(sugar);
+            });
+        });
+
+        it("should throw an error if the amount is below 0", function () {
+            let sugar = -1;
+            assert.throws(() => {
+                orderBuilder.setSugarChoice(sugar);
+            });
+        });
+
+        it("should throw an error if the cup is null", function () {
+            let sugar = null;
+            assert.throws(() => {
+                orderBuilder.setSugarChoice(sugar);
+            });
+        });
+
+        it("should throw an error if the cup is undefined", function () {
+            let sugar;
+            assert.throws(() => {
+                orderBuilder.setSugarChoice(sugar);
+            });
+        });
+
+    });
+
+
+    describe("getOrder", function () {
+
+        it("should return the order", function () {
+            let drink = new Drink();
+            drink.name = "Test";
             orderBuilder.setDrink(drink);
-        });
-    });
 
-});
-
-
-describe("setCup", function () {
-    let orderBuilder: OrderBuilder;
-
-    beforeEach(function () {
-        orderBuilder = new OrderBuilder();
-    });
-
-    it("should set the cup", function () {
-        let cup = new Cup();
-        orderBuilder.setCup(cup);
-        assert.equal(orderBuilder.getOrder().cup, cup);
-    });
-
-    it("should throw an error if the cup is null", function () {
-        let cup = null;
-        assert.throws(() => {
+            let cup = new Cup();
             orderBuilder.setCup(cup);
-        });
-    });
 
-    it("should throw an error if the cup is undefined", function () {
-        let cup;
-        assert.throws(() => {
-            orderBuilder.setCup(cup);
-        });
-    });
-
-});
-
-
-describe("setCupChoice", function () {
-    let orderBuilder: OrderBuilder;
-
-    beforeEach(function () {
-        orderBuilder = new OrderBuilder();
-    });
-
-    it("should set the cup choice boolean", function () {
-        let cupChoice = true;
-        orderBuilder.setCupChoice(cupChoice);
-        assert.equal(orderBuilder.getOrder().bought_cup, cupChoice);
-
-        cupChoice = false;
-        orderBuilder.setCupChoice(cupChoice);
-        assert.equal(orderBuilder.getOrder().bought_cup, cupChoice);
-    });
-
-    it("should throw an error if the choice is null", function () {
-        let cupChoice = null;
-        assert.throws(() => {
+            let cupChoice = true;
             orderBuilder.setCupChoice(cupChoice);
+
+            let sugar = 2;
+            orderBuilder.setSugarChoice(sugar);
+
+            let order = orderBuilder.getOrder();
+
         });
-    });
-
-    it("should throw an error if the choice is undefined", function () {
-        let cupChoice;
-        assert.throws(() => {
-            orderBuilder.setCupChoice(cupChoice);
-        });
-    });
-
-});
-
-
-describe("setSugarChoice", function () {
-    let orderBuilder: OrderBuilder;
-
-    beforeEach(function () {
-        this.orderBuilder = new OrderBuilder();
-    });
-
-    it("should set the sugar amount", function () {
-        let sugar = 2;
-        this.orderBuilder.setSugarChoice(sugar);
-        assert.equal(this.orderBuilder.getOrder().sugarAmount, sugar);
-    });
-
-    it("should throw an error if the amount is above 5", function () {
-        let sugar = 6;
-        assert.throws(() => {
-            this.orderBuilder.setSugarChoice(sugar);
-        });
-    });
-
-    it("should throw an error if the amount is below 0", function () {
-        let sugar = -1;
-        assert.throws(() => {
-            this.orderBuilder.setSugarChoice(sugar);
-        });
-    });
-
-    it("should throw an error if the cup is null", function () {
-        let sugar = null;
-        assert.throws(() => {
-            this.orderBuilder.setSugarChoice(sugar);
-        });
-    });
-
-    it("should throw an error if the cup is undefined", function () {
-        let sugar;
-        assert.throws(() => {
-            this.orderBuilder.setSugarChoice(sugar);
-        });
-    });
-
-});
-
-
-describe("getOrder", function () {
-    let orderBuilder: OrderBuilder;
-
-    beforeEach(function () {
-        orderBuilder = new OrderBuilder();
-    });
-
-    it("should return the order", function () {
-        let drink = new Drink();
-        drink.name = "Test";
-        orderBuilder.setDrink(drink);
-
-        let cup = new Cup();
-        orderBuilder.setCup(cup);
-
-        let cupChoice = true;
-        orderBuilder.setCupChoice(cupChoice);
-
-        let sugar = 2;
-        orderBuilder.setSugarChoice(sugar);
-
-        let order = orderBuilder.getOrder();
 
     });
 
--- tests/ViewTests.ts
+++ tests/ViewTests.ts
@@ -13,121 +13,119 @@ import {ConsoleView} from "../src/view/view";
 
 //var sandbox = sinon.createSandbox();
 
-let test;
-let view : ConsoleView;
 
-describe("Yes/No Question function tests", function () {
 
+
+describe("ConsoleView", function() {
+
+    let view : ConsoleView;
 
     beforeEach(function () {
         sinon.stub(readLineSync, "question");
         view = new ConsoleView();
         //prevent console.log from printing
-        test = sinon.stub(console, "log");
+        sinon.stub(console, "log");
     });
 
     afterEach(function () {
         readLineSync.question.restore();
-        test.restore();
+        sinon.restore();
     });
 
-    it("Should return true when y is written", function () {
-        readLineSync.question.returns("y");
-        let result = view.yesNoQuestion("Do you want to continue?");
-        assert.equal(result, true);
-    });
+    describe("Yes/No Question function tests", function () {
 
-    it("Should return false when n is written", function () {
-        readLineSync.question.returns("n");
-        let result = view.yesNoQuestion("Do you want to continue?");
-        assert.equal(result, false);
-    });
+        it("Should return true when y is written", function () {
+            readLineSync.question.returns("y");
+            let result = view.yesNoQuestion("Do you want to continue?");
+            assert.equal(result, true);
+        });
 
-    it("Should throw an error when a wrong command is written", function () {
-        readLineSync.question.returns("Hello");
-        assert.throws( () => view.yesNoQuestion("Do you want to continue?"), Error);
-    });
+        it("Should return false when n is written", function () {
+            readLineSync.question.returns("n");
+            let result = view.yesNoQuestion("Do you want to continue?");
+            assert.equal(result, false);
+        });
 
-})
+        it("Should throw an error when a wrong command is written", function () {
+            readLineSync.question.returns("Hello");
+            assert.throws( () => view.yesNoQuestion("Do you want to continue?"), Error);
+        });
 
-describe("Range questions function tests", function () {
+    })
 
-    beforeEach(function () {
-        sinon.stub(readLineSync, "question");
-        //prevent console.log from printing
-        test = sinon.stub(console, "log");
-    });
+    describe("Range questions function tests", function () {
 
-    afterEach(function () {
-        readLineSync.question.restore();
-        test.restore();
-    });
+        it("Should return a number when a number is written", function () {
+            readLineSync.question.returns("3");
+            let result = view.rangeQuestion("How much X do you want (1-10)?", 1, 10);
+            assert.equal(result, 3);
+        });
 
-    it("Should return a number when a number is written", function () {
-        readLineSync.question.returns("3");
-        let result = view.rangeQuestion("How much X do you want (1-10)?", 1, 10);
-        assert.equal(result, 3);
-    });
+        it("Should throw an error when a too big number is written", function () {
+            readLineSync.question.returns("15");
+            assert.throws( ()=> view.rangeQuestion("How much X do you want (1-10)?",1,10), Error);
+        });
 
-    it("Should throw an error when a too big number is written", function () {
-        readLineSync.question.returns("15");
-        assert.throws( ()=> view.rangeQuestion("How much X do you want (1-10)?",1,10), Error);
-    });
+        it("Should throw an error when a too small number is written", function () {
+            readLineSync.question.returns("0");
+            assert.throws( ()=> view.rangeQuestion("How much X do you want (1-10)?",1,10), Error);
+        });
 
-    it("Should throw an error when a too small number is written", function () {
-        readLineSync.question.returns("0");
-        assert.throws( ()=> view.rangeQuestion("How much X do you want (1-10)?",1,10), Error);
-    });
+        /*
+        it("Should throw an error when a negative number is written", function () {
+            readLineSync.question.returns("-5");
+            //call rangeQuestion and assert that an error is thrown
+            assert.throws( ()=>rangeQuestion("How much X do you want (1-10)?",1,10), Error);
+        });
+        */
 
-    /*
-    it("Should throw an error when a negative number is written", function () {
-        readLineSync.question.returns("-5");
-        //call rangeQuestion and assert that an error is thrown
-        assert.throws( ()=>rangeQuestion("How much X do you want (1-10)?",1,10), Error);
-    });
-    */
+    })
 
-})
+    describe("Ask Question function tests", function () {
+        before(function () {
+            this.randomstrings = new Map<string, number>([
+                ["numero1", 1],
+                ["numero2", 2],
+                ["numero3", 3],
+            ]);
+        });
 
-describe("Ask Question function tests", function () {
-    before(function () {
-        this.randomstrings = new Map<string, number>([
-            ["numero1", 1],
-            ["numero2", 2],
-            ["numero3", 3],
-        ]);
-    });
+        it("Should return a number when a number is written", function () {
+            readLineSync.question.returns("2");
+            let result : number = view.choiceQuestion<number>("What do you want?", this.randomstrings);
+            assert.equal(result, this.randomstrings[1]);
+        });
 
-    beforeEach(function () {
-        sinon.stub(readLineSync, "question");
-        //prevent console.log from printing
-        test = sinon.stub(console, "log");
-    });
+        it("Should throw an error when a wrong command is written", function () {
+            readLineSync.question.returns("Hello");
+            assert.throws(() => view.choiceQuestion<number>("What do you want?", this.randomstrings), Error);
+        });
 
-    afterEach(function () {
-        readLineSync.question.restore();
-        test.restore();
-    });
+        it("Should throw an error when a too big number is written", function () {
+            readLineSync.question.returns("6");
+            assert.throws(() => view.choiceQuestion<number>("What do you want?", this.randomstrings), Error);
+        });
 
-    it("Should return a number when a number is written", function () {
-        readLineSync.question.returns("2");
-        let result : number = view.choiceQuestion<number>("What do you want?", this.randomstrings);
-        assert.equal(result, this.randomstrings[1]);
-    });
+        it("Should throw an error when a too small number is written", function () {
+            readLineSync.question.returns("0");
+            assert.throws(() => view.choiceQuestion("What do you want?", this.randomstrings), Error);
+        });
 
-    it("Should throw an error when a wrong command is written", function () {
-        readLineSync.question.returns("Hello");
-        assert.throws(() => view.choiceQuestion<number>("What do you want?", this.randomstrings), Error);
-    });
+    })
 
-    it("Should throw an error when a too big number is written", function () {
-        readLineSync.question.returns("6");
-        assert.throws(() => view.choiceQuestion<number>("What do you want?", this.randomstrings), Error);
-    });
 
-    it("Should throw an error when a too small number is written", function () {
-        readLineSync.question.returns("0");
-        assert.throws(() => view.choiceQuestion("What do you want?", this.randomstrings), Error);
-    });
+    describe("displayMessage function tests", function () {
+        it("Should print the message", function () {
+            view.displayMessage("Hello");
+            sinon.assert.calledWith(console.log, "\x1b[32m"+"Hello"+"\x1b[0m");
+        });
 
-})
+        it("Should throw an error if null is passed", function () {
+            assert.throws(() => view.displayMessage(null), Error);
+        });
+
+        it("Should throw an error if the message is empty", function () {
+            assert.throws(() => view.displayMessage(""), Error);
+        });
+    })
+});
