4ebc4ad6071509f07cafd2ce9321d4770215d4d2
Noé Pécault <noepec1@gmail.com>
2022-01-28 11:17:44 +0100

    test: Created OrderController test suite

--- tests/OrderControllerTests.ts
+++ tests/OrderControllerTests.ts
@@ -11,8 +11,9 @@ import {TypeORMService} from "../src/service/TypeORMService";
 import {IService} from "../src/service/IService";
 import {ConsoleView} from "../src/view/view";
 import {SinonMock, SinonStubbedInstance} from "sinon";
+import {describe} from "mocha";
 
-describe("getDrinkSelection", function () {
+describe("OrderController unit tests", () => {
     let controller : OrderController;
     let service : SinonStubbedInstance<IService>;
     let view : ConsoleView;
@@ -29,44 +30,46 @@ describe("getDrinkSelection", function () {
         sinon.restore();
     });
 
-    it("Should return the correct drink when a correct number is entered given a filled list", function () {
-        let drink = new Drink();
-        drink.id = 0;
-        drink.name = "Test drink";
-        drink.price = 10;
-        drink.content = "That drink TESTS good.";
+    describe("getDrinkSelection", function () {
+        it("Should return the correct drink when a correct number is entered given a filled list", function () {
+            let drink = new Drink();
+            drink.id = 0;
+            drink.name = "Test drink";
+            drink.price = 10;
+            drink.content = "That drink TESTS good.";
 
-        let drink1 = new Drink();
-        drink1.id = 0;
-        drink1.name = "Another console drink";
-        drink1.price = 20;
-        drink1.content = "This is TESTY.";
+            let drink1 = new Drink();
+            drink1.id = 0;
+            drink1.name = "Another console drink";
+            drink1.price = 20;
+            drink1.content = "This is TESTY.";
 
-        viewMock.expects("choiceQuestion").once().returns(drink);
+            viewMock.expects("choiceQuestion").once().returns(drink);
 
-        let choice = controller.getDrinkSelection([drink, drink1]);
+            let choice = controller.getDrinkSelection([drink, drink1]);
 
-        assert.equal(choice, drink);
-        viewMock.verify();
+            assert.equal(choice, drink);
+            viewMock.verify();
+        });
+
+        it("Should throw an exception when given an empty list", function () {
+            viewMock.expects("choiceQuestion").never();
+
+            assert.throws(() => { controller.getDrinkSelection([]) });
+
+            viewMock.verify();
+        });
+
+        it("Should throw an exception when given an uninitialized list", function () {
+            viewMock.expects("choiceQuestion").never();
+
+            assert.throws(() => { controller.getDrinkSelection(null) });
+
+            viewMock.verify();
+        });
     });
 
-    it("Should throw an exception when given an empty list", function () {
-        viewMock.expects("choiceQuestion").never();
+    describe("getSizeSelection", function () {
 
-        assert.throws(() => { controller.getDrinkSelection([]) });
-
-        viewMock.verify();
     });
-
-    it("Should throw an exception when given an uninitialized list", function () {
-        viewMock.expects("choiceQuestion").never();
-
-        assert.throws(() => { controller.getDrinkSelection(null) });
-
-        viewMock.verify();
-    });
-});
-
-describe("getSizeSelection", function () {
-
-});
+})
